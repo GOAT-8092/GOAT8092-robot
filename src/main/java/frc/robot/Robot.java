0fabe1a311ec5e9d06de8c41ebc666029719c428
Toprak Soylu <topraksoylu@vosb.k12.tr>
2025-04-17 15:04:06 +0300

    Sabitler sınıfı oluşturuldu ve joystick ile motor portları güncellendi

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+// Gerekli kütüphaneler
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
@@ -17,165 +18,152 @@ import org.opencv.core.Mat;
 import com.studica.frc.AHRS;
 import com.studica.frc.AHRS.NavXComType;
 
+import frc.robot.Sabitler;
+
 public class Robot extends TimedRobot {
 
+    // Motor ve sensör tanımlamaları
     private MecanumDrive mecanumDrive;
     private XboxController controller;
     private PWMVictorSPX elevatorMotor;
-    private PWMVictorSPX elevatorMotorLeft;
     private PWMVictorSPX coralMotor;
     private PWMSparkMax algMotorLeft;
     private PWMSparkMax algMotorRight;
     private AHRS ahrs;
     private Encoder elevatorEncoder;
 
-    // Declare drive motors as instance variables
+    // Sürücü motorları
     private PWMVictorSPX frontLeft;
     private PWMVictorSPX rearLeft;
     private PWMVictorSPX frontRight;
     private PWMVictorSPX rearRight;
-    
-    // private AHRS ahrs;
 
+    // Alg tekeri kontrolü için değişkenler
     private boolean isAlgRunning = false;
     private double algStartTime = 0.0;
 
-    private final double TURN_DEADZONE = 0.15; // Analog çubuğu hassasiyeti
-    private final double TURN_SPEED = 0.6; // Dönüş hızı
+    // Dönüş hassasiyeti (kullanılmıyor, örnek olarak bırakıldı)
+    private final double TURN_DEADZONE = 0.15;
+    private final double TURN_SPEED = 0.6;
 
     @Override
     public void robotInit() {
+        // NavX sensörü başlat
         ahrs = new AHRS(NavXComType.kMXP_SPI);
-        
-        // Initialize drive motors
-        frontLeft = new PWMVictorSPX(constants.FRONT_LEFT_MOTOR_PORT);
-        rearLeft = new PWMVictorSPX(constants.REAR_LEFT_MOTOR_PORT);
-        frontRight = new PWMVictorSPX(constants.FRONT_RIGHT_MOTOR_PORT);
-        rearRight = new PWMVictorSPX(constants.REAR_RIGHT_MOTOR_PORT);
-        
+
+        // Sürücü motorlarını başlat
+        frontLeft = new PWMVictorSPX(Sabitler.ON_SOL_MOTOR_PORT);
+        rearLeft = new PWMVictorSPX(Sabitler.ARKA_SOL_MOTOR_PORT);
+        frontRight = new PWMVictorSPX(Sabitler.ON_SAG_MOTOR_PORT);
+        rearRight = new PWMVictorSPX(Sabitler.ARKA_SAG_MOTOR_PORT);
+
         frontLeft.setSafetyEnabled(false);
         frontRight.setSafetyEnabled(false);
         rearLeft.setSafetyEnabled(false);
         rearRight.setSafetyEnabled(false);
-        
-        // Initialize other motors
-        elevatorMotor = new PWMVictorSPX(constants.ELEVATOR_MOTOR_RIGHT_PORT);
-        elevatorMotorLeft = new PWMVictorSPX(constants.ELEVATOR_MOTOR_LEFT_PORT);
 
-        // Encoder
-        // elevatorEncoder = new Encoder(0, 1,false,EncodingType.k4X);
-        // elevatorEncoder.setDistancePerPulse(1.0/400.0);
-        // elevatorEncoder.reset();
+        // Diğer motorları başlat
+        elevatorMotor = new PWMVictorSPX(Sabitler.ASANSOR_MOTOR_PORT);
+        coralMotor = new PWMVictorSPX(Sabitler.MERCAN_MOTOR_PORT);
+        algMotorRight = new PWMSparkMax(Sabitler.ALG_MOTOR_SAG_PORT);
+        algMotorLeft = new PWMSparkMax(Sabitler.ALG_MOTOR_SOL_PORT);
 
-        coralMotor = new PWMVictorSPX(constants.CORAL_MOTOR_PORT);
-        algMotorRight = new PWMSparkMax(constants.ALG_MOTOR_RIGHT_PORT);
-        algMotorLeft = new PWMSparkMax(constants.ALG_MOTOR_LEFT_PORT);
-       
+        // Motor yönlerini ayarla
+        elevatorMotor.setInverted(Sabitler.ASANSOR_MOTOR_TERS);
+        frontLeft.setInverted(Sabitler.ON_SOL_MOTOR_TERS);
+        rearLeft.setInverted(Sabitler.ARKA_SOL_MOTOR_TERS);
+        frontRight.setInverted(Sabitler.ON_SAG_MOTOR_TERS);
+        rearRight.setInverted(Sabitler.ARKA_SAG_MOTOR_TERS);
 
-        // Motor inversion (if necessary)
-        elevatorMotorLeft.setInverted(constants.ELEVATOR_MOTOR_LEFT_INVERTED);
-        elevatorMotor.setInverted(constants.ELEVATOR_MOTOR_RIGHT_INVERTED);
-        
-        frontLeft.setInverted(constants.FRONT_LEFT_MOTOR_INVERTED);
-        rearLeft.setInverted(constants.REAR_LEFT_MOTOR_INVERTED);
-        frontRight.setInverted(constants.FRONT_RIGHT_MOTOR_INVERTED);
-        rearRight.setInverted(constants.REAR_RIGHT_MOTOR_INVERTED);
-
-        // Motor Followers
-        elevatorMotor.addFollower(elevatorMotorLeft);
-
-        // Create MecanumDrive
+        // Mecanum sürüş sistemi oluştur
         mecanumDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
 
-        // Initialize Xbox Controller
-        controller = new XboxController(constants.JOYSTICK_PORT);
-
-
-        
+        // Joystick başlat
+        controller = new XboxController(Sabitler.JOYSTICK_PORT);
     }
+
     @Override
     public void robotPeriodic() {
-        // SmartDashboard.putNumber("ElevatorEncoder", getElevatorHeight());
-        SmartDashboard.putNumber("Angle", ahrs.getAngle());
+        // Sensör verilerini SmartDashboard'a yaz
+        SmartDashboard.putNumber("Açı", ahrs.getAngle());
         SmartDashboard.putNumber("Yaw", ahrs.getYaw());
         SmartDashboard.putNumber("Roll", ahrs.getRoll());
     }
 
+    // Ölü bölge uygulama fonksiyonu
     private double applyDeadzone(double value) {
-        return (Math.abs(value) > constants.DEADZONE) ? value : 0;
+        return (Math.abs(value) > Sabitler.OLU_BOLGE) ? value : 0;
     }
 
     @Override
     public void teleopInit() {
-        // Teleop Initialization
-        //elevatorEncoder.reset();
+        // Teleop başlatılırken yapılacak işlemler
         LimelightHelpers.setPipelineIndex("", 0);
     }
 
     @Override
     public void teleopPeriodic() {
         // Sürüş kontrolleri
-        double y = controller.getRawAxis(1); // Y ekseni (ileri/geri)
-        double x = -controller.getRawAxis(0); // X ekseni (sol/sağ kayma)
-        double z = controller.getRawAxis(2); // 360 derece dönüş artık axis 2'de
+        double y = controller.getRawAxis(Sabitler.SOL_Y_EKSEN); // Sol analog Y (ileri/geri)
+        double x = -controller.getRawAxis(Sabitler.SOL_X_EKSEN); // Sol analog X (sol/sağ kayma)
+        double z = controller.getRawAxis(Sabitler.SAG_X_EKSEN); // Sağ analog X (360 derece dönüş)
 
-        // Deadzone uygula (isteğe bağlı)
+        // Ölü bölge uygula
         y = applyDeadzone(y);
         x = applyDeadzone(x);
         z = applyDeadzone(z);
 
+        // Mecanum sürüş
         mecanumDrive.driveCartesian(y, x, z);
 
-        // Asansör kontrolü SADECE LT ve RT ile yapılır
-        double lt = controller.getRawAxis(constants.LEFT_TRIGGER);  // LT: aşağı
-        double rt = controller.getRawAxis(constants.RIGHT_TRIGGER); // RT: yukarı
-        double elevatorSpeed = 0;
-        if (rt > 0.1) {
-            elevatorSpeed = rt; // yukarı
-        } else if (lt > 0.1) {
-            elevatorSpeed = -lt; // aşağı
+        // Asansör kontrolü sadece LT ve RT butonları ile yapılır
+        boolean lt = controller.getRawButton(Sabitler.LT_BUTON);  // LT buton (asansör aşağı)
+        boolean rt = controller.getRawButton(Sabitler.RT_BUTON);  // RT buton (asansör yukarı)
+        double asansorHiz = 0;
+        if (rt) {
+            asansorHiz = Sabitler.ASANSOR_HIZI; // yukarı
+        } else if (lt) {
+            asansorHiz = -Sabitler.ASANSOR_HIZI; // aşağı
         } else {
-            elevatorSpeed = 0;
+            asansorHiz = 0;
         }
-        elevatorMotor.set(elevatorSpeed);
-        SmartDashboard.putNumber("Elevator Speed", elevatorSpeed);
+        elevatorMotor.set(asansorHiz);
+        SmartDashboard.putNumber("Asansör Hızı", asansorHiz);
 
-        // Coral System
-        boolean lb = controller.getRawButton(constants.LB);
-        boolean rb = controller.getRawButton(constants.RB);
-        double coralSpeed = 0.0;
+        // Mercan sistemi kontrolü
+        boolean lb = controller.getRawButton(Sabitler.SOL_TAMPON);
+        boolean rb = controller.getRawButton(Sabitler.SAG_TAMPON);
+        double mercanHiz = 0.0;
         if (rb) {
-            coralSpeed = constants.CORAL_SPEED_LIMIT;
+            mercanHiz = Sabitler.MERCAN_HIZ_LIMIT;
         } else if (lb) {
-            coralSpeed = -constants.CORAL_SPEED_LIMIT;
+            mercanHiz = -Sabitler.MERCAN_HIZ_LIMIT;
         }
-        else{
-            coralSpeed = 0;
-        }
-        coralMotor.set(coralSpeed);
-        SmartDashboard.putNumber("Coral Speed", coralSpeed);
+        coralMotor.set(mercanHiz);
+        SmartDashboard.putNumber("Mercan Hızı", mercanHiz);
 
-        // Alg Wheel Control
-        boolean aButton = controller.getRawButtonPressed(constants.BUTTON_A); // A Button
-        boolean bButton = controller.getRawButtonPressed(constants.BUTTON_B); // B Button
+        // Alg tekeri kontrolü
+        boolean aButton = controller.getRawButtonPressed(Sabitler.BUTON_A);
+        boolean bButton = controller.getRawButtonPressed(Sabitler.BUTON_B);
 
         if (aButton) {
             isAlgRunning = true;
             algStartTime = Timer.getFPGATimestamp();
-            algMotorRight.set(constants.ALG_SPEED_LIMIT);
-            algMotorLeft.set(constants.ALG_SPEED_LIMIT);
+            algMotorRight.set(Sabitler.ALG_HIZ_LIMIT);
+            algMotorLeft.set(Sabitler.ALG_HIZ_LIMIT);
             controller.setRumble(RumbleType.kBothRumble, 0.6);
         } else if (bButton) {
             isAlgRunning = true;
             algStartTime = Timer.getFPGATimestamp();
-            algMotorRight.set(-constants.ALG_SPEED_LIMIT);
-            algMotorLeft.set(-constants.ALG_SPEED_LIMIT);
+            algMotorRight.set(-Sabitler.ALG_HIZ_LIMIT);
+            algMotorLeft.set(-Sabitler.ALG_HIZ_LIMIT);
             controller.setRumble(RumbleType.kBothRumble, 0.6);
         }
 
+        // Alg tekeri çalışma süresi kontrolü
         if (isAlgRunning) {
             double currentTime = Timer.getFPGATimestamp();
-            if (currentTime - algStartTime >= constants.ALG_RUN_TIME) {
+            if (currentTime - algStartTime >= Sabitler.ALG_CALISMA_SURESI) {
                 algMotorRight.set(0);
                 algMotorLeft.set(0);
                 isAlgRunning = false;
@@ -183,8 +171,8 @@ public class Robot extends TimedRobot {
             }
         }
     }
-    
 
+    // Otonom değişkenler
     double autonomousTime;
     boolean coralInit = true;
     boolean isCoralRunning = false;
@@ -193,23 +181,25 @@ public class Robot extends TimedRobot {
     
     @Override
     public void autonomousInit() {
-        // Autonomous Initialization
+        // Otonom başlatılırken yapılacak işlemler
         LimelightHelpers.setPipelineIndex("", 1);
         autonomousTime = Timer.getFPGATimestamp();
     }
 
     @Override
     public void autonomousPeriodic() {
+        // Otonom periyodik fonksiyonu
         double currentTime = Timer.getFPGATimestamp();
 
         if (currentTime - autonomousTime <= 1) {
-            mecanumDrive.driveCartesian(-constants.AUTONOMUS_ROBOT_SPEED, 0, 0);
-            SmartDashboard.putNumber("Autonomus Robot Moving", 1);
-            
+            // Otonomda robotu ileri hareket ettir
+            mecanumDrive.driveCartesian(-Sabitler.OTOMATIK_ROBOT_HIZI, 0, 0);
+            SmartDashboard.putNumber("Otonom Robot Hareketi", 1);
         }
         else{
+            // Otonomda robotu durdur
             mecanumDrive.driveCartesian(0, 0, 0);
-            SmartDashboard.putNumber("Autonomus Robot Moving", 0);
+            SmartDashboard.putNumber("Otonom Robot Hareketi", 0);
         }
     }
 }
---
+++ src/main/java/frc/robot/Sabitler.java
@@ -0,0 +1,62 @@
+package frc.robot;
+
+// Sabitler ve portlar burada gruplanmıştır.
+public class Sabitler {
+
+    // ------------------ Joystick Ayarları ------------------
+    // Joystick portu
+    public static final int JOYSTICK_PORT = 0;
+
+    // Butonlar (Logitech F310 için)
+    public static final int BUTON_A = 1; // A tuşu
+    public static final int BUTON_B = 2; // B tuşu
+    public static final int BUTON_X = 3; // X tuşu
+    public static final int BUTON_Y = 4; // Y tuşu
+    public static final int SOL_TAMPON = 5;   // LB (Sol bumper)
+    public static final int SAG_TAMPON = 6;   // RB (Sağ bumper)
+    public static final int SOL_ANALOG_TUS = 9;  // Sol analog stick tuşu
+    public static final int SAG_ANALOG_TUS = 10; // Sağ analog stick tuşu
+    public static final int LT_BUTON = 7; // LT buton portu
+    public static final int RT_BUTON = 8; // RT buton portu
+
+    // Analog eksenler (Logitech F310 için)
+    public static final int SOL_X_EKSEN = 0;   // Sol analog X (sağ -1, sol 1)
+    public static final int SOL_Y_EKSEN = 1;   // Sol analog Y (geri -1, ileri 1)
+    public static final int SAG_X_EKSEN = 2;   // Sağ analog X (sağ -1, sol 1)
+    public static final int SAG_Y_EKSEN = 3;   // Sağ analog Y (geri -1, ileri 1)
+
+    // ------------------ Motor Portları ------------------
+    public static final int ON_SOL_MOTOR_PORT = 4;
+    public static final int ARKA_SOL_MOTOR_PORT = 1;
+    public static final int ON_SAG_MOTOR_PORT = 2;
+    public static final int ARKA_SAG_MOTOR_PORT = 3;
+
+    public static final int ASANSOR_MOTOR_PORT = 6; // Sadece bir port kullanılacak
+    public static final int MERCAN_MOTOR_PORT = 7;
+    public static final int ALG_MOTOR_SOL_PORT = 5;
+    public static final int ALG_MOTOR_SAG_PORT = 8;
+
+    // ------------------ Motor Yönleri ------------------
+    public static final boolean ON_SOL_MOTOR_TERS = true;
+    public static final boolean ON_SAG_MOTOR_TERS = false;
+    public static final boolean ARKA_SOL_MOTOR_TERS = true;
+    public static final boolean ARKA_SAG_MOTOR_TERS = true;
+    public static final boolean ASANSOR_MOTOR_TERS = false; // Tek motor için
+
+    // ------------------ Hız ve Limitler ------------------
+    public static final double MERCAN_HIZ_LIMIT = 0.5;
+    public static final double ALG_HIZ_LIMIT = 1.0;
+    public static final double ALG_CALISMA_SURESI = 1.0;
+    public static final double MERCAN_CALISMA_SURESI = 2.0;
+    public static final double ASANSOR_OTONOM_YUKSEKLIK = 10.0; 
+
+    // ------------------ Diğer Sabitler ------------------
+    public static final double OLU_BOLGE = 0.1;
+    public static final double APRIL_TAG_MESAFE = 5.0;
+    public static final double OTOMATIK_ROBOT_HIZI = 0.5;
+    public static final double OTOMATIK_DONUS_HIZI = 0.02;
+    public static final double ASANSOR_HIZI = 0.4;
+    public static final double ASANSOR_YARICAP = 0.675;
+    public static final double ASANSOR_MAX_YUKSEKLIK = 250.0;
+}
+
